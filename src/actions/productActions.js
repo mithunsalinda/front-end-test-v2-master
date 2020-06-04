fb59177b44dafedf1d4e4bae176161523902ada4
mithun <mithun.salinda.j@gmail.com>
2020-06-05 00:41:37 +0800

    fix deploy

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,7 +1,7 @@
 import { FETCH_PRODUCTS } from "../types";
 import { FILTER_PRODUCTS_TYPE } from "../types";
 export const fetchProducts = () => async (dispatch) => {
-  const res = await fetch("http://localhost:3000/products");
+  const res = await fetch("http://mithunsalinda.github.io/front-end-test-v2-master/src/mocks/list.json");
   const data = await res.json();
   console.log(data);
   dispatch({
