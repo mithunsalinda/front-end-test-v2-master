dc3eb8b0d4d65ea7007abb8ba1b5bdc70c73256b
mithun <mithun.salinda.j@gmail.com>
2020-06-04 18:27:20 +0800

    Updates

---
+++ precache-manifest.d45ac97e3c082692c79b4b4959310a24.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1536b689d8166ac713d4e5999a5fa94",
+    "url": "/front-end-test-v2-master/index.html"
+  },
+  {
+    "revision": "76309dbcd44f8981e771",
+    "url": "/front-end-test-v2-master/static/css/main.f34cca6d.chunk.css"
+  },
+  {
+    "revision": "29d8995826bec0903c98",
+    "url": "/front-end-test-v2-master/static/js/2.14665023.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/front-end-test-v2-master/static/js/2.14665023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76309dbcd44f8981e771",
+    "url": "/front-end-test-v2-master/static/js/main.4ad7c3ca.chunk.js"
+  },
+  {
+    "revision": "67d5a93676674daa959b",
+    "url": "/front-end-test-v2-master/static/js/runtime-main.4d6665f7.js"
+  },
+  {
+    "revision": "299981eac1577858acba31589d7ccb0d",
+    "url": "/front-end-test-v2-master/static/media/mb-star.299981ea.svg"
+  },
+  {
+    "revision": "8fb3c5c8a257d9068667376149d8b5dd",
+    "url": "/front-end-test-v2-master/static/media/mb-wordmark.8fb3c5c8.svg"
+  },
+  {
+    "revision": "5fa9f82410133c555bff8ae742b4df1c",
+    "url": "/front-end-test-v2-master/static/media/spinner.5fa9f824.svg"
+  }
+]);
